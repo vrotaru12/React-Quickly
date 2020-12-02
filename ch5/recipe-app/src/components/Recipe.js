7e1e7b63432148a4132317bd3e7474711e1ac815
Victoria Rotaru <victoriarotaru1996@gmail.com>
2020-12-02 14:08:43 +0000

    Update Recipe.js

--- ch5/recipe-app/src/components/Recipe.js
+++ ch5/recipe-app/src/components/Recipe.js
@@ -1,6 +1,7 @@
 import React from "react";
 import IngredientsList from "./IngredientsList";
 import Instructions from "./Instructions";
+import Summary from "./Summary";
 
 export default function Recipe({
   name = "untitled",
@@ -10,6 +11,7 @@ export default function Recipe({
   return (
     <section id={name.toLowerCase().replace(/ /g, "-")}>
       <h1>{name}</h1>
+      <Summary title="Cooking Instructions" ingredients="6" steps="4"/>
       <IngredientsList list={ingredients} />
       <Instructions title="Cooking Instructions" steps={steps} />
     </section>
